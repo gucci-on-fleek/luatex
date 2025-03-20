20e8a3b5a76ae80caff8ed98973bd4e58c56e8c0
Luigi Scarso <luigi.scarso@gmail.com>
2025-03-20 10:00:17 +0100

    some more never used modes made obsolete + staircase kerns adapted (H.Hagen). LuaTeX 1.23.1

--- source/texk/web2c/luatexdir/luatex.c
+++ source/texk/web2c/luatexdir/luatex.c
@@ -33,8 +33,8 @@
 */
 
 int luatex_version = 123;
-int luatex_revision = '0';
-const char *luatex_version_string = "1.23.0";
+int luatex_revision = '1';
+const char *luatex_version_string = "1.23.1";
 const char *engine_name = my_name;
 
 #include <kpathsea/c-ctype.h>
